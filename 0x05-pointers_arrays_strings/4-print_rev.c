a3ecdb4ecc160f2579cea8d767d6eebcb6115612
beba2123 <amanonymous578@gmail.com>
2022-06-14 14:54:01 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_rev - prints string in reverse
+* @s: pointer to string
+*
+* Return: void
+*/
+void print_rev(char *s)
+{
+int a, b;
+
+while (*(s + a) != 0)
+{
+a++;
+}
+for (b = a - 1; b >= 0; b--)
+{
+_putchar(s[b]);
+}
+_putchar('\n');
+}i
