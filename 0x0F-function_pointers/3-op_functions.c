0a0ac95c031e3221249501c788a71cdbb4143d5a
beba2123 <amanonymous578@gmail.com>
2022-07-06 04:41:50 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,72 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * op_add - addition
+ * @a: sumnd 1
+ * @b: sumnd 2
+ * Return: rsult
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * op_sub - subtraction
+ * @a: minuend
+ * @b: sustrnd
+ * Return: result
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
+ * op_mul - multiplication
+ * @a: multp 1
+ * @b: multp 2
+ * Return: result
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
+ * op_div - division
+ * @a: divisr
+ * @b: dividndo
+ * Return: reult
  */
+
 int op_div(int a, int b)
 {
-	if (b == 0)
+	if (b)
 	{
-		printf("Error\n");
-		exit(100);
+		return (a / b);
 	}
-
-	return (a / b);
+	printf("Error\n");
+	exit(100);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * op_mod - modulo
+ * @a: vaper 1
+ * @b: vaper 2
+ * Return: result
  */
+
 int op_mod(int a, int b)
 {
-	if (b == 0)
+	if (b)
 	{
-		printf("Error\n");
-		exit(100);
+		return (a % b);
 	}
-
-	return (a % b);
+	printf("Error\n");
+	exit(100);
 }
