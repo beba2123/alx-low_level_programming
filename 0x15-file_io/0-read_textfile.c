806e68edd41d10a7f8501253a7a6c593b4ae8d70
beba2123 <amanonymous578@gmail.com>
2022-07-25 08:59:07 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,32 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ *read_textfile - reads a text file and prints it to the
+ *POSIX standard output
+ *@filename:pointer to file description
+ *@letters:number of letters it should read and print
+ *Return:actual no. of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	int fd, bytes;
+	char *buf;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+/*check if pointer is NULL or letters < 1*/
+	if (filename == NULL)
+		return (0);
+	if (letters < 1)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	fd = open(filename, O_RDONLY);
+/*if the process is not successful*/
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	bytes = read(fd, buf, letters);
+	buf[bytes + 1] = '\0';
+
+	write(STDOUT_FILENO, buf, bytes);
+	close(fd);
+	free(buf);
+	return (bytes);
 }
